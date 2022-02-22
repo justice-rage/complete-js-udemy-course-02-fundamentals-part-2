d744fcaab54193c977bbdba806d809e822020365
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-22 15:11:37 -0800

    complete lecture 35

--- starter/script.js
+++ starter/script.js
@@ -35,16 +35,33 @@
 // Lecture: Function Declarations vs Expressions
 
 // Function declaration
-const age1 = calcAge1(1991); // works
+// const age1 = calcAge1(1991); // works
 
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// // Function expression
+// const age2 = calcAge2(1991); // doesn't work
+
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+// console.log(age1, age2);
+
+// Lecture 35: Arrow functions
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years.`
 }
 
-// Function expression
-const age2 = calcAge2(1991); // doesn't work
-
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
-}
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1991, 'Justice'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
