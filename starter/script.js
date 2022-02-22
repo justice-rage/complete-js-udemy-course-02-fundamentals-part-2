cd3a7b34033caa20eabe7f251bd1a07989d34e9f
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-22 14:45:33 -0800

    complete lecture 33

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,33 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log(`I can drive.`);
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log(`I can drive.`);
 
 // const interface = 'Audio';
 // const private = 534;
 // const if = 23;
+
+function logger() {
+  console.log(`My name is Justice.`);
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+fruitProcessor(5, 0);
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5,0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice)
