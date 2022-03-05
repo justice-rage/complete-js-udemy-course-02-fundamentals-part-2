b0d6c23d7ad28a139b5724cb30476a20801b2f8d
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-05 13:32:09 -0800

    complete assignment #10 - lecture: iteration: the for loop

--- starter/assignments.js
+++ starter/assignments.js
@@ -269,3 +269,16 @@
 
 // console.log(myCountry.checkIsland());
 // console.log(myCountry.isIsland);
+
+///////////////////////////////////////
+
+// Assignment #10
+// Lecture: Iteration: The for Loop
+
+// 1. There are elections in your country! In a small town, there are only 50 voters. Use a for loop to simulate the 50 people voting, by logging a string like this to the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
+
+const voters = 50;
+
+for (index = 1; index <= voters; index++) {
+  console.log(`Voter number ${index} is currently voting.`)
+}
