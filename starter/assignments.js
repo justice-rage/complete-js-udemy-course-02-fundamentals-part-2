dbf93e598d7709d810af015bc4ce0bd2b3b06d19
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-05 10:43:12 -0800

    comment out code

--- starter/assignments.js
+++ starter/assignments.js
@@ -5,17 +5,17 @@
 
 // 1. Write a function called 'describeCountry' which takes three parameters: 'country', 'population' and 'capitalCity'. Based on this input, the function returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'
 
-describeCountry = function(country, population, capitalCity) {
-  return `${country} has ${population} million people and its capital city is ${capitalCity}`
-}
+// describeCountry = function(country, population, capitalCity) {
+//   return `${country} has ${population} million people and its capital city is ${capitalCity}`
+// }
 
 // 2. Call this function 3 times, with input data for 3 different countries.Store the returned values in 3 different variables, and log them to the console
 
-let usa = describeCountry('United States of America', 375, 'Washington D.C.');
-let finland = describeCountry('Finland', 6, 'Helsinki');
-let mexico = describeCountry('Mexico', 128, 'Mexico City')
+// let usa = describeCountry('United States of America', 375, 'Washington D.C.');
+// let finland = describeCountry('Finland', 6, 'Helsinki');
+// let mexico = describeCountry('Mexico', 128, 'Mexico City')
 
 
-console.log(usa);
-console.log(finland);
-console.log(mexico);
+// console.log(usa);
+// console.log(finland);
+// console.log(mexico);
