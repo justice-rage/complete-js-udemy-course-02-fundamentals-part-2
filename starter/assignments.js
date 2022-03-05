9dd320885e1ebeb56bbe66b46f017b79cf6d1c62
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-05 11:38:54 -0800

    complete assignment #5 - lecture: introduction to arrays

--- starter/assignments.js
+++ starter/assignments.js
@@ -104,3 +104,32 @@
 // console.log(china);
 // console.log(usa);
 // console.log(finland);
+
+/////////////////////////////
+
+// Assignment #5
+// Lecture: Introduction to Arrays
+
+// 1. Create an array containing 4 population values of 4 countries of your choice. You may use the values you have been using previously. Store this array into a variable called 'populations'
+
+let populations = [1441, 375, 6, 175];
+
+// 2. Log to the console whether the array has 4 elements or not (true or false)
+
+console.log(populations.length)
+
+if(populations.length === 4) {
+  console.log(`true`);
+} else {
+  console.log(`false`)
+}
+
+// 3. Create an array called 'percentages' containing the percentages of the world population for these 4 population values. Use the function 'percentageOfWorld1' that you created earlier to compute the 4 percentage values
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100
+}
+
+let percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]), percentageOfWorld1(populations[2]), percentageOfWorld1(populations[3])];
+
+console.log(percentages);
