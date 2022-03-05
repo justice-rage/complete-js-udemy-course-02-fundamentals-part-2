d84e9643a424ee554fbdfd76e8f2ed42ed348363
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-05 11:17:35 -0800

    comment out code

--- starter/assignments.js
+++ starter/assignments.js
@@ -89,18 +89,18 @@
 
 // 2. To calculate the percentage,'describePopulation' call the 'percentageOfWorld1' you created earlier
 
-let percentageOfWorld = (population) => (population / 7900) * 100;
+// let percentageOfWorld = (population) => (population / 7900) * 100;
 
-describePopulation = function(country, population) {
-  return `${country} has ${population} million people, which is about ${percentageOfWorld(population)} of the world`;
-}
+// describePopulation = function(country, population) {
+//   return `${country} has ${population} million people, which is about ${percentageOfWorld(population)} of the world`;
+// }
 
 // 3. Call 'describePopulation' with data for 3 countries of your choice
 
-let china = describePopulation('china', 1441);
-let usa = describePopulation('usa', 375);
-let finland = describePopulation('finland', 6);
+// let china = describePopulation('china', 1441);
+// let usa = describePopulation('usa', 375);
+// let finland = describePopulation('finland', 6);
 
-console.log(china);
-console.log(usa);
-console.log(finland);
+// console.log(china);
+// console.log(usa);
+// console.log(finland);
