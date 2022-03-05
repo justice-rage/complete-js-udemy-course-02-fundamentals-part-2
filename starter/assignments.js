0a4fcf6c99a8987e163e8679eebc8cf01cd1ed76
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-05 13:18:52 -0800

    complete assignment #9 - lecture: object methods

--- starter/assignments.js
+++ starter/assignments.js
@@ -219,3 +219,53 @@
 // myCountry['population'] -= 2;
 
 // console.log(myCountry);
+
+//////////////////////////////////
+
+// Assignment #9
+// Lecture: Object Methods
+
+// 1. Add a method called 'describe' to the 'myCountry' object. This method will log a string to the console, similar to the string logged in the previous assignment, but this time using the 'this' keyword.
+
+// myCountry = {
+//   country: 'USA',
+//   capital: 'Washington DC',
+//   language: 'english',
+//   population: 375,
+//   neighbours: ['Canada', 'Mexico'],
+
+//   describe: function() {
+//     return (`${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`);
+//   }
+// }
+
+// 2. Call the 'describe' method
+
+// console.log(myCountry.describe());
+
+// 3. Add a method called 'checkIsland' to the 'myCountry' object. This method will set a new property on the object, called 'isIsland'. 'isIsland' will be true if there are no neighbouring countries, and false if there are. Use the ternary operator to set the property.
+
+myCountry = {
+  country: 'USA',
+  capital: 'Washington DC',
+  language: 'english',
+  population: 375,
+  neighbours: ['Canada', 'Mexico'],
+
+  describe: function() {
+    return (`${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`);
+  },
+
+  // checkIsland: function() {
+  //   this.isIsland = this.neighbours.length === 0;
+  //   return this.isIsland;
+  // }
+  
+  checkIsland: function() {
+    this.isIsland = (this.neighbours.length === 0) ? true : false;
+    return this.isIsland;
+  }
+}
+
+console.log(myCountry.checkIsland());
+console.log(myCountry.isIsland);
